63d8525a7fd5edc11e910ecbfba6527902517acd
Nathan Price-Lee <nathan@edassess.net>
2024-12-07 17:46:44 -0800

    rename feud app

--- apps/feud-test/jest.config.ts
+++ apps/feud-test/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
-  displayName: 'feud',
+  displayName: 'feud-test',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/feud',
+  coverageDirectory: '../../coverage/apps/feud-test',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
---
+++ apps/feud-test/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
+  },
+];
