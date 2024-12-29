667c0d4b424581ca06f3a21f17d77b5da9b7a8de
Nathan Price-Lee <nathan@edassess.net>
2024-12-28 23:48:53 -0800

    question

--- libs/nw/feature-feud/src/lib/feud.component.ts
+++ libs/nw/feature-feud/src/lib/feud.component.ts
@@ -17,10 +17,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
 import { ToolbarService } from '@nathan-and-winnie/feature-toolbar';
 import { WindowComponent } from '@nathan-and-winnie/ui-window';
-import { Team } from './data/_types';
+import { Question, Round, Team } from './data/_types';
 import { hf24 } from './data/hf24';
 
 @Component({
@@ -51,11 +51,17 @@ export class FeudComponent implements OnInit {
 
   games = [hf24];
   password = '';
+  tabIndex = 0;
   teams: Team[] = [
     { name: 'Team 1', score: 0 },
     { name: 'Team 2', score: 0 },
   ];
-  tabIndex = 0;
+  
+  activeAnswers: boolean[] = [];
+  activePot = 0;
+  activeRound: Round | null = null;
+  activeTeam: Team | null = null;
+  activeQuestion: Question | null = null;
 
   params = toSignal(this.route.params);
   currGame = computed(() =>
@@ -84,6 +90,35 @@ export class FeudComponent implements OnInit {
 
   reopenWindow() {
     this.window()?.reopenWindow();
+  }
+
+  deactivate(event?: MatTabChangeEvent) {
+    if (event) this.tabIndex = event.index;
+    this.activeQuestion = null;
+    this.activeAnswers = [];
+    this.activeRound = null;
+    this.activeTeam = null;
+    this.activePot = 0;
+  }
+
+  activate(round: Round, question: Question, team: Team | null = null) {
+    this.activeAnswers = question.answers.map(() => false);
+    this.activeQuestion = question;
+    this.activeRound = round;
+    this.activeTeam = team;
+    this.activePot = 0;
+  }
+
+  toggleAnswer(i: number) {
+    if (!this.activeQuestion || !this.activeRound) return;
+    this.activeAnswers[i] = !this.activeAnswers[i];
+    this.activePot += (this.activeAnswers[i] ? 1 : -1) * this.activeQuestion.answers[i].points * this.activeRound.multiplier;
+    if (this.activePot) this.yesAudio.play();
+  }
+
+  award(team: Team) {
+    team.score += this.activePot;
+    this.yesAudio.play();
   }
 
   ngOnInit() {
