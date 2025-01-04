432bc7aa5e55555044b671cf514767fac4636176
Nathan Price-Lee <nathan@edassess.net>
2025-01-04 14:51:53 -0800

    import ricky

--- libs/nw/feature-ricky/src/lib/ricky.component.ts
+++ libs/nw/feature-ricky/src/lib/ricky.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, viewChild, viewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { ToolbarService } from '@nathan-and-winnie/feature-toolbar';
 
 @Component({
   selector: 'lib-ricky',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './ricky.component.html',
   styleUrl: './ricky.component.scss',
 })
-export class RickyComponent {}
+export class RickyComponent implements OnInit {
+  toolbar = inject(ToolbarService);
+
+  content = viewChild<ElementRef>('content');
+  headings = viewChildren<ElementRef>('heading');
+
+  getOffset(heading: ElementRef) {
+    return (heading.nativeElement.tagName[1] - 1) * 16;
+  }
+
+  scrollTo(heading: ElementRef, index: number) {
+    if (index === 0) {
+      this.content()?.nativeElement.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      heading.nativeElement.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+
+  ngOnInit() {
+    this.toolbar.patch(1, {
+      icon: 'pets',
+      label: 'Ricky',
+      title: 'Ricky',
+      route: '/ricky',
+    });
+  }
+}
