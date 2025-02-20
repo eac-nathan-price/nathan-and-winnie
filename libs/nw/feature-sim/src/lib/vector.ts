04320dc7179d784840778b5a6b11848633fe9e64
Nathan Price-Lee <nathan@edassess.net>
2025-02-19 17:06:58 -0800

    transform cells

--- libs/nw/feature-sim/src/lib/vector.ts
+++ libs/nw/feature-sim/src/lib/vector.ts
@@ -3,6 +3,59 @@ export class Vector2 {
     public x: number,
     public y: number
   ) {}
+
+  toVector3(z = 0) {
+    return new Vector3(this.x, this.y, z);
+  }
+
+  static distance(a: Vector2, b: Vector2) {
+    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+  }
+
+  static lerp(a: Vector2, b: Vector2, t: number) {
+    return new Vector2(
+      a.x + (b.x - a.x) * t,
+      a.y + (b.y - a.y) * t
+    );
+  }
+
+  static normalize(a: Vector2) {
+    const length = Math.sqrt(a.x ** 2 + a.y ** 2);
+    return new Vector2(a.x / length, a.y / length);
+  }
+
+  static nudge(a: Vector2, b: Vector2, d: number) {
+    const direction = Vector2.normalize(Vector2.subtract(b, a));
+    return Vector2.add(a, Vector2.multiply(direction, d));
+  }
+
+  static add(a: Vector2, b: Vector2) {
+    return new Vector2(
+      a.x + b.x,
+      a.y + b.y
+    );
+  }
+
+  static subtract(a: Vector2, b: Vector2) {
+    return new Vector2(
+      a.x - b.x,
+      a.y - b.y
+    );
+  }
+
+  static multiply(v: Vector2, scalar: number) {
+    return new Vector2(
+      v.x * scalar,
+      v.y * scalar
+    );
+  }
+
+  static divide(v: Vector2, scalar: number) {
+    return new Vector2(
+      v.x / scalar,
+      v.y / scalar
+    );
+  }
 }
 
 export class Vector3 {
@@ -11,6 +64,64 @@ export class Vector3 {
     public y: number,
     public z: number
   ) {}
+
+  toVector2() {
+    return new Vector2(this.x, this.y);
+  }
+
+  static distance(a: Vector3, b: Vector3) {
+    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2);
+  }
+
+  static lerp(a: Vector3, b: Vector3, t: number) {
+    return new Vector3(
+      a.x + (b.x - a.x) * t,
+      a.y + (b.y - a.y) * t,
+      a.z + (b.z - a.z) * t
+    );
+  }
+
+  static normalize(a: Vector3) {
+    const length = Math.sqrt(a.x ** 2 + a.y ** 2 + a.z ** 2);
+    return new Vector3(a.x / length, a.y / length, a.z / length);
+  }
+
+  static nudge(a: Vector3, b: Vector3, d: number) {
+    const direction = Vector3.normalize(Vector3.subtract(b, a));
+    return Vector3.add(a, Vector3.multiply(direction, d));
+  }
+
+  static add(a: Vector3, b: Vector3) {
+    return new Vector3(
+      a.x + b.x,
+      a.y + b.y,
+      a.z + b.z
+    );
+  }
+
+  static subtract(a: Vector3, b: Vector3) {
+    return new Vector3(
+      a.x - b.x,
+      a.y - b.y,
+      a.z - b.z
+    );
+  }
+
+  static multiply(v: Vector3, scalar: number) {
+    return new Vector3(
+      v.x * scalar,
+      v.y * scalar,
+      v.z * scalar
+    );
+  }
+
+  static divide(v: Vector3, scalar: number) {
+    return new Vector3(
+      v.x / scalar,
+      v.y / scalar,
+      v.z / scalar
+    );
+  }
 }
 
 export type Bounds2 = [
@@ -85,11 +196,19 @@ export class Random {
     );
   }
 
+  static vector2s(box: Box2, count: number) {
+    return Array.from({ length: count }, () => Random.vector2(box));
+  }
+
   static vector3(box: Box3) {
     return new Vector3(
       Random.float(box.xMin, box.xMax),
       Random.float(box.yMin, box.yMax),
       Random.float(box.zMin, box.zMax)
     );
+  }
+
+  static vector3s(box: Box3, count: number) {
+    return Array.from({ length: count }, () => Random.vector3(box));
   }
 }
